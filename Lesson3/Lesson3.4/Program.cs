3efdea71ba2cfd1abe371514e1f1facfe02e9e55
tyrhaton <719107@student.inholland.nl>
2025-02-27 15:54:06 +0100

    Added Lesson3.4

---
+++ Lesson3/Lesson3.4/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace Lesson3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[!] Hello, World!");
+            Stack MyStack = new Stack();
+
+            String Commando = "";
+            while (Commando != "Stop")
+            {
+                Console.Write("[?] Give Command: ");
+                Commando = Console.ReadLine();
+                if (Commando == "Push")
+                {
+                    try
+                    {
+                        Console.Write("[?] Give Value: ");
+                        MyStack.Push(Console.ReadLine());
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("[-] " + e.Message);
+                    }
+                    catch (SystemException e)
+                    {
+                        Console.WriteLine("[-] " + e.Message);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("[+] Continue...");
+                    }
+
+                }
+                else if (Commando == "Pop")
+                {
+                    StackElement element = MyStack.Pop();
+                    if(element != null)
+                    {
+                        Console.WriteLine("[+] Element Data: ");
+                        element.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("[-] Stack is Empty");
+                    }
+                }
+                else if (Commando == "Print")
+                {
+                    MyStack.Print();
+                }
+                else if (Commando == "ReversePrint")
+                {
+                    MyStack.ReversePrint();
+                }
+                else if (Commando == "Amount")
+                {
+                    Console.WriteLine(MyStack.Data + " element(s)");
+                }
+                else if (Commando == "Search")
+                {
+                    try
+                    {
+                        Console.Write("[?] Give Value: ");
+                        MyStack.Search(Console.ReadLine());
+
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("[-] " + e.Message);
+                    }
+                    catch (SystemException e)
+                    {
+                        Console.WriteLine("[-] " + e.Message);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("[+] Continue...");
+                    }
+                }
+                else if (Commando == "PrintDate")
+                {
+                    DateTime now = DateTime.Now;
+                    MyStack.Print(now);
+                }
+                else if (Commando == "EnQueue")
+                {
+                    Console.Write("[?] Give a value for the queue: ");
+                    MyStack.EnQueue(Console.ReadLine());
+                }
+                else if (Commando == "DeQueue")
+                {
+                    StackElement last = MyStack.DeQueue();
+                    if(last != null)
+                    {
+                        last.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("[-] Queue is empty");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[-] Invalid Command");
+                }
+
+            }
+        }
+    }
+}
---
+++ Lesson3/Lesson3.4/Stack.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lesson3._4
+{
+    internal class Stack
+    {
+        public int data = 0;
+        StackElement Top = null;
+        public int Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public void Push(string data)
+        {
+            Data++;
+
+            try
+            {
+                int getal = int.Parse(data);
+                IntContent content = new IntContent(getal);
+                Top = new StackElement(content, Top);
+            }
+            catch
+            {
+                StringContent content = new StringContent(data);
+                Top = new StackElement(content, Top);
+            }
+        }
+
+        public StackElement Pop()
+        {
+            if (Top != null)
+            {
+                StackElement element = Top;
+                Top = Top.Next;
+                Data--;
+                return element;
+            }
+            else
+            {
+                Console.WriteLine("[-] Stack is empty");
+                return null;
+            }
+        }
+
+        public void Print(DateTime date)
+        {
+            Top.Print(date);
+        }
+        public void Print()
+        {
+            Top.Print();
+        }
+
+        public void ReversePrint()
+        {
+            Top.Print();
+        }
+
+        public void Search(string value)
+        {
+            try
+            {
+                bool includes = Top.Search(value);
+
+                if (includes)
+                {
+                    Console.WriteLine("[+] Element is in stack!");
+                }
+                else
+                {
+                    Console.WriteLine("[-] Element is not in stack!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("[-] There are no elements in the stack.");
+            }
+        }
+
+        public void EnQueue(string value)
+        {
+            Push(value);
+        }
+
+        public StackElement DeQueue()
+        {
+            if (Top == null) return null;
+            if (Top.IsLast())
+            {
+                StackElement Last = Top;
+                Top = null;
+                return Last;
+            }
+            return Top.DeQueue();
+        }
+    }
+}
